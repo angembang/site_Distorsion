41dd6508209f4aebda9564253c39f67067d2f6da
angembang <132391052+angembang@users.noreply.github.com>
2024-02-13 15:42:50 +0100

    Initial commit

---
+++ services/RandomStringGenerator.php
@@ -0,0 +1,15 @@
+<?php
+class RandomStringGenerator
+{
+    public function generate(int $length = 10): string
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $string = '';
+
+        for ($i = 0; $i < $length; ++$i) {
+            $string .= $chars[random_int(0, 61)];
+        }
+
+        return $string;
+    }
+}
